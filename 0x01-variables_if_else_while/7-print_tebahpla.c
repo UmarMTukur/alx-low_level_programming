6e69acd71a1780bb08525b86068fe779604be80a
UmarMTukur <umartukur55.com>
2022-06-03 17:28:30 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letters;
+for (letters = 122; letters >= 97; letters--)
+{
+putchar(letters);
+}
+putchar('\n');
+return (0);
+}
