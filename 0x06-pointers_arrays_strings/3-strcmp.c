71b43a04b7ac6f9d845a1c0b88b94ccd23035282
UmarMTukur <umartukur55.com>
2022-06-16 18:32:52 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string to be compered
+ * @s2: string to be compered
+ * Return: negative if s1 <s2; 0 if matchig and positive int if s1 > s2
+ */
+ int _strcmp(char "s1, cahr "s2)
+{
+	int x;
+
+	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x])
+			return (s1[x] - s2[x]);
+	}
+	return (0);
+
+}
