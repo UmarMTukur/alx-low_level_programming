8666c6470177dd627373b1f5de0a76846f2da888
UmarMTukur <umartukur55.com>
2022-06-20 10:18:48 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,32 +1,14 @@
 #include "main.h"
 /**
- * _strspn - gets the legth of a prefix substring
- * @s: char string array
- * @accept: char array to check bytes with
- * Return: Number of bytes in the initial segment of s
+ * *_strchr - locates a characater in string
+ * @s: pointer to a string
+ * @c: character to be found
+ * Return: pointer to first occurence of c
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	int x, y, z;
-
-	x = 0;
-	z = 0;
-
-	while (s[x] != '\0')
-	{
-		y = 0;
-		while (accept[y] != '\0')
-		{
-			if (s[x] == accept[y])
-			{
-				z++;
-				break;
-			}
-			y++;
-		}
-		if (accept[y] == '\0')
-			break;
-		x++;
-	}
-	return (z);
+	while (*s != c)
+		if (!*s++)
+			return (0);
+	return (s);
 }
